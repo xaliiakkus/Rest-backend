20f66b958e4d3318da130d896c258490a0a241de
Ali Akkuş <90987920+xaliiakkus@users.noreply.github.com>
2023-11-21 00:45:27 +0300

    Update user.js

--- controllers/user.js
+++ controllers/user.js
@@ -5,45 +5,64 @@ const cloudinary = require('cloudinary').v2;
 const crypto = require('crypto');
 const nodemailer = require('nodemailer');
 
-// create a new user
-const register = async (req, res) => {
-    const avatar = await cloudinary.uploader.upload(req.body.avatar, {
-        folder: "avatars",
-        width: 100,
-        crop: 'scale'
-    });
-    const { name, email, password } = req.body;
-    const user = await User.findOne({ email })
-    if (user) {
-        return res.status(404).json({ errors: [{ msg: 'Kullanıcı Mevcut' }] })
-    }
-    const passwordHahs = await bcrypt.hash(password, 10)
 
-    if (password.length < 6) {
-        return res.status(404).json({ errors: [{ msg: 'Şifre en az 6 karakter olmalıdır' }] })
-    }
-    const newUser = await User.create({
+
+const register = async (req, res) => {
+    try {
+      // Upload avatar to Cloudinary
+      const avatarResponse = await cloudinary.uploader.upload(req.body.avatar, {
+        folder: "avatars",
+        width: 130,
+        crop: 'scale',
+        timestamp: Math.floor(Date.now() / 1000),
+      });
+  
+      const { name, email, password } = req.body;
+  
+      // Hash password
+      const passwordHash = await bcrypt.hash(password, 10);
+  
+      // Check password length
+      if (password.length < 6) {
+        return res.status(400).json({ errors: [{ msg: 'Şifre en az 6 karakter olmalıdır' }] });
+      }
+  
+      // Check if user already exists
+      const existingUser = await User.findOne({ email });
+      if (existingUser) {
+        return res.status(400).json({ errors: [{ msg: 'Kullanıcı Mevcut' }] });
+      }
+  
+      // Create a new user
+      const newUser = await User.create({
         name,
         email,
-        password: passwordHahs,
+        password: passwordHash,
         avatar: {
-            public_id: avatar.public_id,
-            url: avatar.secure_url
+          public_id: avatarResponse.public_id,
+          url: avatarResponse.secure_url
         }
-    })
-    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, { expiresIn: '1h' })
-
-    const cookieOptions = {
+      });
+  
+      // Generate JWT token
+      const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+  
+      // Set cookie options
+      const cookieOptions = {
         httpOnly: true,
         expires: new Date(Date.now() + 5 * 24 * 60 * 60 * 1000),
+      };
+  
+      // Send response
+      res.status(201).cookie("token", token, cookieOptions).json({
+        user: newUser,
+        token: token
+      });
+    } catch (error) {
+      console.error('Error during user registration:', error);
+      res.status(500).json({ errors: [{ msg: 'Internal Server Error' }] });
     }
-
-    res.status(201).cookie("token", token, cookieOptions).json(
-        newUser,
-        token
-    )
-}
-
+  };
 // login user
 const login = async (req, res) => {
     const { email, password } = req.body;
@@ -152,7 +171,7 @@ const resetPassword = async (req, res) => {
     
     await user.save();
 
-    const token = jwt.sign({id:user._id}, process.env.SECRET_TOKEN, {expiresIn:"1h"})
+    const token = jwt.sign({id:user._id}, process.env.JWT_SECRET, {expiresIn:"1h"})
 
     const cookieOptions = {
         httpOnly: true,
