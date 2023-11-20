80b36b3e925277b58f8aef7aa0dce0608f3a9bd3
Ali Akkuş <90987920+xaliiakkus@users.noreply.github.com>
2023-11-21 01:03:09 +0300

    Update user.js
    New

--- models/user.js
+++ models/user.js
@@ -12,24 +12,23 @@ const UserSchema = mongoose.Schema({
         unique: true
     },
     password: {
-        type: Number,
+        type: Number || String,
         required: true,
         minLength: 6
     },
     avatar: {
         public_id: {
             type: String,
-            required: true,
+           
         },
         url: {
             type: String,
-            required: true,
+       
         }
     },
     role: {
         type: String,
         default: "user",
-        required:true
     },
     resetPasswordToken: {
         type: String
