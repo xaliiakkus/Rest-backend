20b272e109db62ba26794abf548b5cc89a94d6a9
Ali Akkuş <90987920+xaliiakkus@users.noreply.github.com>
2023-11-22 01:23:28 +0300

    Update user.js

--- controllers/user.js
+++ controllers/user.js
@@ -191,7 +191,7 @@ const resetPassword = async (req, res) => {
 }
 
 const userDetails = async (req, res, next) => {
-    const user = await User.findById(req.params.id)
+    const user = await User.findById(req.user.id)
     res.status(200).json({
         user
     })
