32e095bd04f644f271dab1aa23c30e2444331eb2
Ali Akkuş <90987920+xaliiakkus@users.noreply.github.com>
2023-11-23 21:21:06 +0300

    Update user.js

--- controllers/user.js
+++ controllers/user.js
@@ -191,14 +191,10 @@ const resetPassword = async (req, res) => {
 }
 
 const userDetails = async (req, res, next) => {
- try {
-       const user = await User.findById(req.user.id)
+       const user = await User.findById(req.user)
        res.status(200).json({
            user
        })
- } catch (error) {
-    res.status(error).error(404).json({ errors: [{ msg: 'User Details Not Found' }] });
- }
 }
 
 module.exports = {
